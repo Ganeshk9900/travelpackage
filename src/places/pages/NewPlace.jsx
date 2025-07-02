705c03f46a6ba7016c5147c6d0e69ab508ee2e79
ganesh.frendz <ganesh.frendz@gmail.com>
2025-07-02 18:43:22 +0000

    my second commit

---
+++ src/places/pages/NewPlace.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NewPlace=()=>{
+    return(
+        <> 
+        <h2> Welcome to new places</h2> 
+        
+        </>
+        
+    )
+}
+export default NewPlace;
---
+++ src/users/pages/Users.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const User=()=>{
+    return(
+        <> 
+        <h2> Welcome to Users Components</h2> 
+        </>
+        
+    )
+}
+export default User;
